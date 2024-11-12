8ef16cc178542f9f5e889e37d25f9ace20506b92
Artur Gil Luiz <arturgilluiz752@gmail.com>
2024-11-11 21:01:32 -0300

    Update Mineral.c

--- Mineral.c
+++ Mineral.c
@@ -1,24 +1,30 @@
 #include "Mineral.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void preenche_mineral(TMineral* mineral, char nomeM, int dureza, int reatividade, char cor) {
-    set_nomeM
+TMineral preenche_mineral(TMineral* mineral, char *nomeM, float dureza, float reatividade, char *cor) {
+    set_nomeM(mineral, nomeM);
+    set_dureza(mineral, dureza);
+    set_reatividade(mineral, reatividade);
+    set_cor(mineral, cor);
+
+    return *mineral; 
 }
 
-char get_nomeM(TMineral* mineral) {
+char* get_nomeM(TMineral* mineral) {
     return mineral->nomeM;
 }
 
-int get_dureza(TMineral* mineral) {
+float get_dureza(TMineral* mineral) {
     return mineral->dureza;
 }
 
-int get_reatividade(TMineral* mineral) {
+float get_reatividade(TMineral* mineral) {
     return mineral->reatividade;
 }
 
-char get_cor(TMineral* mineral) {
+char* get_cor(TMineral* mineral) {
     return mineral->cor;
 }
 
@@ -26,11 +32,11 @@ void set_nomeM(TMineral* mineral, char *nomeM) {
     strcpy(mineral->nomeM, nomeM);
 }
 
-void set_dureza(TMineral* mineral, int dureza) {
+void set_dureza(TMineral* mineral, float dureza) {
     mineral->dureza = dureza;
 }
 
-void set_reatividade(TMineral* mineral, int reatividade) {
+void set_reatividade(TMineral* mineral, float reatividade) {
     mineral->reatividade = reatividade;
 }
 
