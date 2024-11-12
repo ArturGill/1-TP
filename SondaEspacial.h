3d2cd93ec369b79625707f6e43c4ef01a2631a2d
Artur Gil Luiz <arturgilluiz752@gmail.com>
2024-11-11 21:03:40 -0300

    Update SondaEspacial.h

--- SondaEspacial.h
+++ SondaEspacial.h
@@ -1,17 +1,41 @@
 #ifndef SONDAESPACIAL_H
 #define SONDAESPACIAL_H
 
+typedef enum{
+    LIGADA = 1,
+    DESLIGADA = 0
+} StatusSonda;
+
 typedef struct {
-    char latlong[30];
+    int identificador;
+    float latSonda;
+    float longSonda;
+    int compartimento;
+    StatusSonda status; //perguntar se os nomes do especificados no TP tem que ser os mesmos
+
     int capacidadeSonda;
     int velSonda;
     int combustivelSonda;
 } SondaMarte;
 
-void set_Latlong(SondaMarte* sonda, char latlong, int velSonda, int combustivelSonda);
+void inicializaSonda(SondaMarte* sonda); 
+void imprimeSonda(SondaMarte sonda);
+void ligaSonda(SondaMarte* sonda);
+void desligaSonda(SondaMarte* sonda);
+void moveSonda(SondaMarte* sonda);
+
+
+void setIdentificador(SondaMarte* sonda, int identificador);
+void set_long(SondaMarte* sonda, float latSonda);
+void set_long(SondaMarte* sonda, float longSonda);
+void set_compartimento(SondaMarte* sonda, int compartimento);
+void set_status(SondaMarte* sonda, StatusSonda status);
+
+
 void set_CapacidadeSonda(SondaMarte* sonda, int capacidadeSonda);
 void set_VelSonda(SondaMarte* sonda, int vel);
 void set_CombustivelSonda(SondaMarte* sonda, int combustivel);
-void inicializaSonda(SondaMarte* sonda, char latlong, int capacidadeSonda);
+
+
 
 #endif // SONDAESPACIAL_H
