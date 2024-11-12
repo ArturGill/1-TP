883c128558db0a4870811b3a0636b982aea92eec
Artur Gil Luiz <arturgilluiz752@gmail.com>
2024-11-11 21:02:57 -0300

    Update Rocha.h

--- Rocha.h
+++ Rocha.h
@@ -1,16 +1,26 @@
+#ifndef ROCHA_H
+#define ROCHA_H
 
+#include "ListaMineral.h"
 
 typedef struct
 {
     int identificador;
-    
+    char categoria[50];
+    TListaMineral lista_mineral;
     double peso;
-    char categoria [50];
     char localizacao[50];
     int data;
-}TRocha;
+} TRocha;
 
-void iniciarRocha(TRocha *rocha, int identificador,,double peso,char categoria,char localizacao,int data);
-void ObterValores(TRocha * );
-void atribuirValores();
-void classificar(TRocha *rocha, char categoria);
+void iniciarRocha(TRocha *rocha, int identificador,double peso,char localizacao,int data);
+double get_peso(TRocha *rocha);
+int get_data(TRocha *rocha);
+char get_localizacao(TRocha *rocha);
+void set_identificador(TRocha *rocha, int indentificador);
+void set_localizacao(TRocha *rocha, char localizacao);
+void set_peso(TRocha *rocha, double peso);
+void set_data(TRocha *rocha, int data);
+const char* classificar(TRocha *rocha, char categoria);
+
+#endif // ROCHA_H
