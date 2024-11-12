42266ca1d380eb1d0d15c807c46db3100bae6f20
Artur Gil Luiz <arturgilluiz752@gmail.com>
2024-11-11 21:02:17 -0300

    Update Mineral.h

--- Mineral.h
+++ Mineral.h
@@ -4,19 +4,19 @@
 typedef struct 
 {
     char nomeM[50], cor[50];
-    int dureza, reatividade;
+    float dureza, reatividade;
 } TMineral;
 
-void preenche_mineral(TMineral* mineral, char nomeM, int dureza, int reatividade, char cor);
+TMineral preenche_mineral(TMineral* mineral, char *nomeM, float dureza, float reatividade, char *cor);
 
-char get_nomeM(TMineral* mineral);
-int get_dureza(TMineral* mineral);
-int get_reatividade(TMineral* mineral);
-char get_cor(TMineral* mineral);
+char* get_nomeM(TMineral* mineral);
+float get_dureza(TMineral* mineral);
+float get_reatividade(TMineral* mineral);
+char* get_cor(TMineral* mineral);
 
-void set_nomeM(TMineral* mineral, char nomeM);
-void set_dureza(TMineral* mineral, int dureza);
-void set_reatividade(TMineral* mineral, int reatividade);
-void set_cor(TMineral* mineral, char cor);
+void set_nomeM(TMineral* mineral, char *nomeM);
+void set_dureza(TMineral* mineral, float dureza);
+void set_reatividade(TMineral* mineral, float reatividade);
+void set_cor(TMineral* mineral, char *cor);
 
 #endif // MINERAL_H
